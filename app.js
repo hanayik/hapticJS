7d32c4450624ce1d1c354a1578a3ce4d979f4a2f
Taylor Hanayik <hanayik@gmail.com>
2017-08-14 14:46:59 -0400

    added platform checking

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ var exec = require('child_process').exec
 var path = require('path')
 var platform = require('platform')
 var cmd = path.join(__dirname, 'hapticJS', 'DerivedData', 'hapticJS', 'Build', 'Products', 'Release', 'hapticJS')
-var os = platform.os
 
 exports.vibrate = function() {
-  if (os.includes('Mac')) {
+  if (os.platform().includes('darwin')) {
     console.log('vibrating now...')
     exec(cmd, function(error, stdout, stderr) {
       // command output is in stdout
